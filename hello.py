05fc2cfc67405a2ee39c5c8bfa7563ad178726a3
Mohammad Goli <gli.mhmd@gmail.com>
2015-07-03 15:24:03 +0430

    AI+ :P

--- hello.py
+++ hello.py
@@ -16,6 +16,23 @@ def hello_world():
 def search(search_query):
 	return "i know im idiot but {}".format(search_query)
 
+#dynamic rout with URL types 
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1 
+	return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
+
+
 if __name__ == "__main__":
 	app.run()
 
