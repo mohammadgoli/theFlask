a0a37fd6eee0970f897f6bc18ec65a83623d61e9
Mohammad Goli <gli.mhmd@gmail.com>
2015-07-03 15:10:58 +0430

    dynamic rout!

--- hello.py
+++ hello.py
@@ -3,11 +3,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/hello")
 
 def hello_world():
 	return "Hey now brown cow"
+
+
+#dynamic route 
+@app.route("/search/<search_query>")
+def search(search_query):
+	return "i know im idiot but {}".format(search_query)
 
 if __name__ == "__main__":
 	app.run()
